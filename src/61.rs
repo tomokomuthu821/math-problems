5da62df4c80011cb2e6a2f13af7276856c8714b3
tomokomuthu821 <tomokomuthu821@gmail.com>
2025-04-09 21:39:40 +0000

    Add 61.rs

---
+++ src/61.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = 5;
+    let y = 10;
+
+    if x < y {
+        println!("x is less than y");
+    } else if x > y {
+        println!("x is greater than y");
+    } else {
+        println!("x and y are equal");
+    }
+}
