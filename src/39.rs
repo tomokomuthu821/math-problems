ef406187bb4412bfd9736b9509800324907184cf
tomokomuthu821 <tomokomuthu821@gmail.com>
2025-04-09 21:12:27 +0000

    Add 39.rs

---
+++ src/39.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Example: Generate some random numbers and perform calculations with them.
+    let num1 = 5.0;
+    let num2 = 3.0;
+    let sum = num1 + num2;
+    println!("The sum of {} and {} is {}", num1, num2, sum);
+}
+
