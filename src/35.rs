fe1f7a32e442eeb144fc77d6249579384f110191
tomokomuthu821 <tomokomuthu821@gmail.com>
2025-04-09 21:07:25 +0000

    Add 35.rs

---
+++ src/35.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // This is an example Rust code snippet.
+    let x = 5;
+    if x > 0 {
+        println!("x is positive.");
+    } else {
+        println!("x is not positive.");
+    }
+}
