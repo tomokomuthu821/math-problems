e67ccd45cda8b58d8eb6d079a491da789a268676
tomokomuthu821 <tomokomuthu821@gmail.com>
2025-04-09 21:24:49 +0000

    Add 49.rs

---
+++ src/49.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Example Rust code snippet
+    let x = 5;
+    if x > 10 {
+        println!("x is greater than 10");
+    } else {
+        println!("x is not greater than 10");
+    }
+}
