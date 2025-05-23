1eeeed01c165aeeb265945d687c742fc1ee4a397
tomokomuthu821 <tomokomuthu821@gmail.com>
2025-05-23 05:51:16 +0000

    Add 77.rs

---
+++ src/77.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    if x > 0 {
+        println!("x is greater than 0");
+    } else if x == 0 {
+        println!("x is zero");
+    } else {
+        println!("x is negative");
+    }
+}
